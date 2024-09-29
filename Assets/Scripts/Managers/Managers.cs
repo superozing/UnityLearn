9507cfd819ab3cc7f5dc52fbda6dd391c3e11104
superozing <amazingozing@gmail.com>
2024-09-29 21:20:10 +0900

    싱글톤 패턴
    스태틱 멤버 이름은 s_를 앞에 붙이는 관례가 있나봐요.
    멤버 getter는 함수를 사용하기 보다는 프로퍼티를 사용해서 깔끔하게 사용할 수 있도록 해주는 게 좋아보여요.
    
    싱글톤 패턴은 클래스 당 객체를 하나만 생성할 수 있도록 해주는 디자인 패턴으로 알고 있는데, 이런 식으로 막는다면 객체를 여러 개 생성할 수 있는 것이 아닌가? 라는 생각이 들어요.
    
    if 조건문에 !obj와 같이 사용해도 null이 아닌지 체크 후 동작해요.
    
    객체를 생성할 때, 프로퍼티 set을 초기화 시점에 호출할 수 있는 것 같아요. 프로퍼티... 좋은데??
    
    DontDestroyOnLoad() 함수는 왠만해서 해당 오브젝트가 삭제되지 않도록 해준다고 해요.

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    static Managers instance; // 유일성이 보장된다.
+    static Managers s_instance; // 유일성이 보장된다.
+    public static Managers Instance { get { Init(); return s_instance; } }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 매니저 객체 가져오기
+        Init();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject obj = GameObject.Find("@Managers");
+
+            if (!obj)
+            {
+                obj = new GameObject { name = "@Managers" };
+                obj.AddComponent<Managers>();
+            }
+
+            // 웬만해서는 삭제되지 않도록 함.
+            DontDestroyOnLoad(obj);
+
+            s_instance = obj.GetComponent<Managers>();
+        }
+
     }
 }
--- Assets/Scripts/Managers/Player.cs
+++ Assets/Scripts/Managers/Player.cs
@@ -7,9 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // 매니저 객체 가져오기
-        GameObject obj = GameObject.Find("@Managers");
-        Managers mgr = obj.GetComponent<Managers>();
+        Managers mgr = Managers.Instance;
     }
 
     // Update is called once per frame
